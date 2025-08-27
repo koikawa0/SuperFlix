a321d5870acc7653288e1459fadffe8bc2fadeef
koikawa0 <kaiofittipaldisc@gmail.com>
2025-08-27 19:39:16 -0300

    commiting

---
+++ proj07b/src/Routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const routes = express.Router();
+
+routes.get("/contents", function (req, ans) {
+
+});
+
+routes.get("/genre", function (req, ans) {
+
+});
+
+routes.get("/content/:code", function (req, ans) {
+
+});
+
+routes.post("/content", function (req, ans) {
+    
+});
+
+export default routes;
---
+++ proj07b/src/database/Connection.js
@@ -0,0 +1,19 @@
+import notifier from "node-notifier";
+import mongoose from "mongoose";
+import "dotenv/config"
+
+const adress = process.env.MONGO_URI;
+const option = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(adress, option)
+    .then 
+    (function(){
+        notifier.notify({
+            title: "proj07",
+            message: "DATABASE CONNECTED",
+            icon: "/"
+        })
+    })
+    .catch(function(error){
+        console.log(error.message)
+    })
